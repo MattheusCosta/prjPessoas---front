00669294049e886e4be2df1bf8622559e3617f83
MattheusCosta <matheuscosta1921@gmail.com>
2019-08-24 02:54:56 +0900

    Sistema de edição e exclusão

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -133,3 +133,76 @@ $(document).on("click", "#editar", function(){
   $("#salvarEdit").css("visibility","visible");
   $("#photo").css("visibility","visible");
 });
+
+//Editar
+
+$(document).on("click","#salvarEdit",function(){
+  if(!$('input[name="arquivo"]').val()){
+      var parametros = {
+        "codigo":$("#codigo").val(),
+        "nome":$("#nome").val(),
+        "idade":$("#idade").val(),
+        "sexo":$("input[name='sexo']:checked").val(),
+        "cidade":$("#cidade").val(),
+        "cpf":$("#cpf").val()
+      }
+
+      $.ajax({
+          type:"post",
+          url:"https://livrosatv.000webhostapp.com/edita.php",
+          data:parametros,
+          success: function(data){
+            navigator.notification.alert("msg:"+data);
+            location.reload();
+          },
+          error: function(data){
+              navigator.notification.alert(data);
+          }
+      });
+  }else{
+    var prop = document.getElementById('caminho').files[0];
+    var nome_imagem = prop.name;
+    var extensao_imagem = nome_imagem.split('.').pop().toLowerCase();
+
+    if(jQuery.inArray(extensao_imagem,['png','jpg','jpeg']) == -1){
+        navigator.notification.alert("imagem invalida");
+    }else{
+        var form_data = new FormData();
+        form_data.append("foto",prop);
+        form_data.append("nome",$("#nome").val());
+        form_data.append("idade",$("#idade").val());
+        form_data.append("endereco",$("#cidade").val());
+        form_data.append("sexo",$("input[name='sexo']:checked").val());
+        form_data.append("cpf",$("#cpf").val());
+        $.ajax({
+        url:"https://livrosatv.000webhostapp.com/editaImagem.php",
+        method:'POST',
+        data:form_data,
+        contentType:false,
+        cache:false,
+        processData:false,
+
+        success:function(data){
+          navigator.notification.alert(data);
+          location.reload();
+        }
+      });
+    }
+  }
+});
+
+//Deletar
+
+$(document).on("click","#excluir",function(){
+  $.ajax({
+        type:"post",
+        url:"https://livrosatv.000webhostapp.com/deleta.php",
+        data:"codigo="+$("#codigo").val(),
+        success: function(data){
+           location.reload();
+        },
+        error: function(data){
+             navigator.notification.alert(data);
+        }
+    });
+});
