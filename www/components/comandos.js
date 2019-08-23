a1fbb32a461f4c4d843977edc5136c5a2e4b201f
MattheusCosta <matheuscosta1921@gmail.com>
2019-08-24 02:56:34 +0900

    Sistema de câmera e scanner

--- www/components/comandos.js
+++ www/components/comandos.js
@@ -206,3 +206,73 @@ $(document).on("click","#excluir",function(){
         }
     });
 });
+
+//camera
+
+$(document).on("click", "#foto", function(){
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI,
+    saveToPhotoAlbum:true,
+    correctOrientation:true
+    });
+
+  function onSuccess(imageURI) {
+      var image = document.getElementById('Imagem registrada com sucesso!');
+  }
+
+  function onFail(message) {
+      alert('Erro ao capturar imagem: ' + message);
+  }
+});
+
+//ações dos buttons de editar e cancelar
+
+$(document).on("click", "#edita", function(){
+  habilita();
+  $("#editar_deletar").css("visibility","hidden");
+  $("#salvar_cancelar").css("visibility","visible");
+});
+
+$(document).on("click", "#cancela", function(){
+  desabilita();
+  $("#editar_deletar").css("visibility","visible");
+  $("#salvar_cancelar").css("visibility","hidden");
+});
+
+function scanBarcode() {
+
+  window.plugins.barcodeScanner.scan(function(result) {
+
+    $.ajax({
+      type:"post",
+      url:"https://prjpessoa.000webhostapp.com/exibirUm.php",
+      data:parametro,
+      dataType:"id="+result.text,
+      success: function(data){
+          $("#codigo").val(data.pessoas.codigo);
+          $("#nome").val(data.pessoas.nome);
+          $("#idade").val(data.pessoas.idade);
+          $("#sexo").val(data.pessoas.sexo);
+          if ($('input[name="sexo"]:checked').val() == "feminino") {
+            $("#radio").prop("checked", true);
+            $("#radio2").prop("checked", false);
+          }
+          else{
+            $("#radio").prop("checked", false);
+            $("#radio2").prop("checked", true);
+          }
+          $("#cidade").val(data.pessoas.cidade);
+          $("#cpf").val(data.pessoas.cpf);
+          $("#imagem").attr('src',"https://livrosatv.000webhostapp.com/"+data.pessoas.imagem);
+      },
+      error: function(data){
+          navigator.notification.alert(data);
+      }
+    });
+
+    }, function(error) {
+      alert("Scanning failed: " + error);
+    }
+  );
+}
